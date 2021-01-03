8b37ab8d861efd140bc92e763ec1dc2139b76b2e
unlocomqx <amxmodx.unloco@gmail.com>
2021-01-03 13:02:08 +0100

    Send current state when on start

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function rxdDoPreprocess(options) {
   function wrapStatement(statement, filename, line_number) {
     const id = makeid(4);
     let details = `{statement: ${JSON.stringify(statement)}, filename: ${JSON.stringify(filename)}, line: ${line_number}, id: "${id}"}`;
-    let start_ev = `{ let svrxd_start = Date.now(); dsp('SvelteReactiveStart', ${details}, svrxd_start);`;
+    let start_ev = `{ let svrxd_start = Date.now(); dsp('SvelteReactiveStart', ${details}, svrxd_start, eval("$$$self.$capture_state()"));`;
     // eval is used to avoid the svelte compiler.
     // $$$ is used because something is replacing $$ with one $
     let end_ev = `dsp('SvelteReactiveEnd', ${details}, svrxd_start, eval("$$$self.$capture_state()")); }`;
