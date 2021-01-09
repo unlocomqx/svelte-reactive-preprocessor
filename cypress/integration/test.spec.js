8ab4c21802871ca7b01424664c0d80078e849539
unlocomqx <amxmodx.unloco@gmail.com>
2021-01-09 20:16:43 +0100

    Test various reactive statements

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -9,8 +9,12 @@ describe("App test", () => {
     .contains("Hello world!")
     .get("#add")
     .click()
-    .get('#result')
-    .contains("Count: 2");
+    .get("#result")
+    .should("contain", "Count: 2")
+    .and("contain", "Double: 4")
+    .and("contain", "Triple: 6")
+    .and("contain", "Half: 1")
+    .and("contain", "Quarter: 0.5");
   });
 
 });
--- tests/app/src/App.svelte
+++ tests/app/src/App.svelte
@@ -1,41 +1,66 @@
 <script>
-	export let name;
+  export let name;
+  export let half;
+  export let quarter = 0;
 
-	let count = 1;
+  let count = 1;
 
-	function increment() {
-		count++;
-	}
+  function increment() {
+    count++;
+  }
+
+  $: double = count * 2;
+
+  let triple;
+  $: triple = count * 3;
+
+  $: half = count / 2;
+
+  $: quarter = count / 4;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<div>
-		<button id="add" on:click={increment}>Add</button>
-		<p id="result">
-			Count: {count}
-		</p>
-	</div>
+  <h1>Hello {name}!</h1>
+  <div>
+    <button id="add" on:click={increment}>Add</button>
+    <div id="result">
+      <div>
+        Count: {count}
+      </div>
+      <div>
+        Double: {double}
+      </div>
+      <div>
+        Triple: {triple}
+      </div>
+      <div>
+        Half: {half}
+      </div>
+      <div>
+        Quarter: {quarter}
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
